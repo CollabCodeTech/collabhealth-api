b7d29631256de052148615e2786b4d9148b0021c
Joviane <joviane@gmail.com>
2019-08-24 18:31:26 +0100

    Creates project with Spring Boot

---
+++ src/main/java/tech/collabcode/collabhealth/CollabhealthApiApplication.java
@@ -0,0 +1,13 @@
+package tech.collabcode.collabhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabhealthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabhealthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/collabcode/collabhealth/CollabhealthApiApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.collabcode.collabhealth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollabhealthApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
