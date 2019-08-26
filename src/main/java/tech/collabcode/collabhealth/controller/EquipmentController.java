115f816c27adc00b40dd93f6f43cbba7e0817403
Joviane <joviane@gmail.com>
2019-08-26 21:44:31 +0100

    Creates equipment search by field(s)

--- src/main/java/tech/collabcode/collabhealth/controller/EquipmentController.java
+++ src/main/java/tech/collabcode/collabhealth/controller/EquipmentController.java
@@ -2,18 +2,22 @@ package tech.collabcode.collabhealth.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import tech.collabcode.collabhealth.model.Equipment;
 import tech.collabcode.collabhealth.repository.EquipmentRepository;
+import tech.collabcode.collabhealth.specification.EquipmentSearchSpecification;
 
 @RestController
 @RequestMapping("/api/equipments")
@@ -30,8 +34,9 @@ public class EquipmentController {
 	}
 
 	@GetMapping
-	public ResponseEntity<List<Equipment>> list() {
-		List<Equipment> allEquipments = this.equipmentRepository.findAll();
+	public ResponseEntity<List<Equipment>> list(@RequestParam(required=false) Map<String, String> queryMap) {
+		Specification<Equipment> equipmentSearch = new EquipmentSearchSpecification(queryMap).build();
+		List<Equipment> allEquipments = this.equipmentRepository.findAll(equipmentSearch);
 		return ResponseEntity.ok(allEquipments);
 	}
 }
--- src/main/java/tech/collabcode/collabhealth/repository/EquipmentRepository.java
+++ src/main/java/tech/collabcode/collabhealth/repository/EquipmentRepository.java
@@ -2,12 +2,13 @@ package tech.collabcode.collabhealth.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import tech.collabcode.collabhealth.model.Equipment;
 
-public interface EquipmentRepository extends Repository<Equipment, Long> {
+public interface EquipmentRepository extends Repository<Equipment, Long>, JpaSpecificationExecutor<Equipment> {
 	Equipment save(Equipment e);
 
 	@Query("select e from Equipment e")
